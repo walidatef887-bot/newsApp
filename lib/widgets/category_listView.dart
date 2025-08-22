c43e852ef0aa8844a0c3c0cb3a395e172215c281
Walid Atef <walidatef@gmail.com>
2025-08-22 08:34:44 +0300

    third

--- lib/widgets/category_listView.dart
+++ lib/widgets/category_listView.dart
@@ -1,21 +1,30 @@
-
-
-
 import 'package:flutter/material.dart';
 import 'package:news_app/models/category_model.dart';
 
 import 'category_card.dart';
 
-class CategoryListView extends StatelessWidget{
-  
+class CategoryListView extends StatelessWidget {
   final List<CategoryModel> categories = [
-    CategoryModel('https://goqii.com/blog/wp-content/uploads/6-Health-Benefits-of-Playing-Sports-1024x683.jpg', 'Sports'),
-    CategoryModel('https://dbppl.com/wp-content/uploads/2020/09/featured-solutions-quadient.jpg', 'Business'),
-    CategoryModel('https://t4.ftcdn.net/jpg/00/53/64/49/360_F_53644926_0mvUCIxCCTvIa7BAIFuUa3xsaNA9lbeb.jpg', 'Science'),
-    CategoryModel('https://img.freepik.com/premium-photo/cardano-blockchain-platform_23-2150411956.jpg', 'Technology'),
-
+    CategoryModel(
+      'https://goqii.com/blog/wp-content/uploads/6-Health-Benefits-of-Playing-Sports-1024x683.jpg',
+      'Sports',
+    ),
+    CategoryModel(
+      'https://dbppl.com/wp-content/uploads/2020/09/featured-solutions-quadient.jpg',
+      'Business',
+    ),
+    CategoryModel(
+      'https://t4.ftcdn.net/jpg/00/53/64/49/360_F_53644926_0mvUCIxCCTvIa7BAIFuUa3xsaNA9lbeb.jpg',
+      'Science',
+    ),
+    CategoryModel(
+      'https://img.freepik.com/premium-photo/cardano-blockchain-platform_23-2150411956.jpg',
+      'Technology',
+    ),
   ];
-  
+
+  String categoryState = 'business';
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -25,10 +34,29 @@ class CategoryListView extends StatelessWidget{
         itemCount: 4,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return CategoryCard(categoryModel: categories[index],);
+          return GestureDetector(
+            onTap: (){
+              if(index==0){
+                categoryState = 'sports';
+                print(categoryState);
+              }
+              else if(index==1){
+                categoryState = 'business';
+                print(categoryState);
+              }
+              else if(index==2){
+                categoryState = 'science';
+                print(categoryState);
+              }
+              else if(index==3){
+                categoryState = 'technology';
+                print(categoryState);
+              }
+            },
+            child: CategoryCard(categoryModel: categories[index]),
+          );
         },
       ),
     );
   }
-
 }
--- lib/widgets/news_list_view.dart
+++ lib/widgets/news_list_view.dart
@@ -3,6 +3,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/models/article_model.dart';
 import 'package:news_app/services/news_services.dart';
+import 'package:news_app/widgets/category_listView.dart';
 import 'news_tile.dart';
 
 class NewsListView extends StatefulWidget{
@@ -18,6 +19,8 @@ class _NewsListViewState extends State<NewsListView> {
   List<ArticleModel> articles = [];
   bool isLoading = true;
 
+  CategoryListView categoryListView = CategoryListView();
+
   @override
   void initState() {
     // TODO: implement initState
@@ -27,10 +30,9 @@ class _NewsListViewState extends State<NewsListView> {
   }
 
   Future<void> getData() async {
-    articles =await NewsServices(Dio()).getNews('business');
+    articles =await NewsServices(Dio()).getNews(categoryListView.categoryState);
     isLoading = false;
     setState(() {
-
     });
   }
 
